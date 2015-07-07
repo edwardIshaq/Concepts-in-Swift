c029ac8008984319b0f43bba6bfe32b83fd1a0c4
Edward Ishaq <edward.ishaq@gmail.com>
2015-07-07 15:34:43 -0700

    - Not able to create a class where it can hold different DataSources - Added a protocol with generic function and a test class -- This concept does not solve the problem since I need to be returning the same type of objects and not any generic type

--- Protocol-Oriented-Programming.playground/Contents.swift
+++ Protocol-Oriented-Programming.playground/Contents.swift
@@ -6,24 +6,89 @@ var str = "Protocol Oriented Programming"
 
 
 protocol DataSource {
-    typealias Element: Hashable
+    typealias Element
     
     var elements: [Element] { get set }
     
-    func elementsSignal() -> [Element]
+    func generateElements() -> [Element]
 }
 
-class AppleContact {
-    var title = ""
+class Person {
+    var title = "No name"
 }
 
-class GoogleContact {
-    var title = ""
+class AppleContact: Person {
+    var contactID: Int
+    init(id: Int, title: String) {
+        self.contactID = id
+
+        super.init()
+        self.title = title
+    }
+}
+
+class GoogleContact: Person {
+    var googleID: String
+    init(id: String, title: String) {
+        self.googleID = id
+        
+        super.init()
+        self.title = title
+    }
+
 }
 
 class AppleDataSource: DataSource {
-    var elements = [AppleContact]()
-    func elementsSignal() -> [AppleContact] {
+    var elements = [Person]()
+    
+    func generateElements() -> [Person] {
+        var generatedContact = AppleContact(id: Int(count(elements)), title: String("Apple #\(self.elements.count)"))
+        elements.append(generatedContact)
         return elements
     }
 }
+
+class GoogleDataSource: DataSource {
+    var elements = [Person]()
+    
+    func generateElements() -> [Person] {
+        var generatedContact = GoogleContact(id: String("googleID:\(self.elements.count)"), title: String("Google #\(self.elements.count)"))
+        elements.append(generatedContact)
+        return elements
+    }
+}
+
+let appleDS = AppleDataSource()
+appleDS.generateElements()
+appleDS.elements
+
+let googleDS = GoogleDataSource()
+googleDS.generateElements()
+appleDS.elements
+
+enum DataSourceType {
+    case Apple, Google
+}
+
+
+
+protocol Source {
+    func elements<T>(t: T) -> [T]
+    //func generateElements<T>() -> [T]
+}
+
+class TestSource: Source {
+
+    func elements<T>(t: T) -> [T] {
+        var arr = [T]()
+        for _ in 0...3 {
+            arr.append(t)
+        }
+        return arr
+    }
+}
+
+let tester = TestSource()
+tester.elements("Hello")
+tester.elements(100)
+tester.elements(Person())
